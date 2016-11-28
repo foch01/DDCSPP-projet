88fa813ad1875c3910bcea894c88b58247e63acd
foch01 <tsanceo@yahoo.com>
2016-11-28 10:59:59 +0100

    reorganization

--- DDCSPP-projet-corrupt/Appli_test/js/description.js
+++
@@ -1,9 +0,0 @@
-var desc = document.getElementsByClassName("open");
-var i;
-
-for (i = 0; i < desc.length; i++) {
-    desc[i].onclick = function(){
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
-  }
-}
--- DDCSPP-projet-corrupt/README.md
+++
@@ -1,1 +0,0 @@
-# DDCSPP-projet
